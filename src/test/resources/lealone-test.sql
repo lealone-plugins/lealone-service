dc6701d403031ea4587e61eeeea5ce9e5f670216
Honghua Zhu <zhh200910@gmail.com>
2026-02-10 23:59:23 +0800

    lealone.yaml替换成lealone-test.sql

---
+++ src/test/resources/lealone-test.sql
@@ -0,0 +1,7 @@
+-- Copyright Lealone Database Group.
+-- Licensed under the Server Side Public License, v 1.
+-- Initial Developer: zhh
+
+create config lealone (
+    base_dir: 'target/test-data'
+)
