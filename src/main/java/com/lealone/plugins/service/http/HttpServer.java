316f64a22024390a154a1f2fd317d793b71e495b
Honghua Zhu <zhh200910@gmail.com>
2026-02-12 16:56:28 +0800

    删除HttpServer.create方法

--- src/main/java/com/lealone/plugins/service/http/HttpServer.java
+++ src/main/java/com/lealone/plugins/service/http/HttpServer.java
@@ -7,8 +7,6 @@ package com.lealone.plugins.service.http;
 
 import java.util.Map;
 
-import com.lealone.common.util.Utils;
-
 public interface HttpServer {
 
     String getWebRoot();
@@ -33,14 +31,4 @@ public interface HttpServer {
 
     void stop();
 
-    public static HttpServer create() {
-        return create(null);
-    }
-
-    public static HttpServer create(String type) {
-        if (type == null || type.equalsIgnoreCase("vertx"))
-            return Utils.newInstance("com.lealone.plugins.vertx.VertxServer");
-        else
-            return Utils.newInstance("com.lealone.plugins.tomcat.TomcatServer");
-    }
 }
--- src/main/java/com/lealone/plugins/service/http/HttpServerEngine.java
+++ src/main/java/com/lealone/plugins/service/http/HttpServerEngine.java
@@ -14,5 +14,4 @@ public abstract class HttpServerEngine extends ProtocolServerEngineBase {
     public HttpServerEngine(String name) {
         super(name);
     }
-
 }
